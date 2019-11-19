ca90b7df7fb40c4b759a4a2539bda6ef9ad0c41a
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-19 11:11:20 +0100

    Dockerfile

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -2,13 +2,12 @@
 
 sudo mkdir -p $DATA_PATH
 sudo mkdir -p $TABLESPACE_PATH
-sudo chown -R docker:docker $TABLESPACE_PATH
 sudo docker run --rm \
     --name pg-docker-$LONG_APP_NAME \
     -e POSTGRES_PASSWORD=$PG_PASSWORD \
     -e LONG_APP_NAME=$LONG_APP_NAME \
     -d -p $DB_PORT:5432 \
     -v $DATA_PATH:/var/lib/postgresql/data \
-    -v $TABLESPACE_PATH:$PGTABLESPACES \
+    -v $TABLESPACE_PATH:/var/lib/postgresql/tablespaces \
     postgres:$LONG_APP_NAME
 
