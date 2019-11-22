1b844afb9942d5373e5860b178a8225d3e4bec21
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-22 14:05:14 +0100

    Scripts

--- docker/scripts/respawn.sh
+++ docker/scripts/respawn.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-source ./setenv.sh
-sudo docker stop pg-docker-$LONG_APP_NAME
-sudo rm -r /app/docker/postgresql/$LONG_APP_NAME
+# WARNING
+# All your data will be LOST
+# Run this script only for development
 
+source ./setenv.sh
+sudo rm -r /app/docker/postgresql/$LONG_APP_NAME
 mkdir -p $HOME/.docker-utils
 cd $HOME/.dockerutils
 sudo rm -r $HOME/.dockerutils/postgresql-utils
 git clone https://github.com/gdeignacio/postgresql-utils.git
+cd $HOME/.dockerutils/postgresql-utils/src
+sudo docker build -t "postgres:$LONG_APP_NAME" .
 cd $HOME/.dockerutils/postgresql-utils/scripts
 sudo chmod +x *
-#source ./setenv.sh
-./run-build.sh
+#./run-build.sh
+sudo mkdir -p $DATA_PATH
 ./run-container.sh
---
+++ docker/scripts/stop-and-run-container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#sudo mkdir -p $DATA_PATH
+sudo docker stop pg-docker-$LONG_APP_NAME
+sudo docker run --rm \
+    --name pg-docker-$LONG_APP_NAME \
+    -e POSTGRES_PASSWORD=$PG_PASSWORD \
+    -e LONG_APP_NAME=$LONG_APP_NAME \
+    -d -p $DB_PORT:5432 \
+    -v $DATA_PATH:/var/lib/postgresql/data \
+    postgres:$LONG_APP_NAME
+
