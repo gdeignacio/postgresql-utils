ce82218439e9ed0fb1ff0363e89ffe8d2f488d31
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-22 15:41:15 +0100

    Path

--- docker/scripts/respawn.sh
+++ docker/scripts/respawn.sh
@@ -7,7 +7,7 @@
 source ./setenv.sh
 sudo rm -r /app/docker/postgresql/$LONG_APP_NAME
 mkdir -p $HOME/.docker-utils
-cd $HOME/.dockerutils
+cd $HOME/.docker-utils
 sudo rm -r $HOME/.docker-utils/postgresql-utils
 git clone https://github.com/gdeignacio/postgresql-utils.git
 cd $HOME/.docker-utils/postgresql-utils/docker/src
@@ -16,4 +16,11 @@ cd $HOME/.docker-utils/postgresql-utils/docker/scripts
 sudo chmod +x *
 #./run-build.sh
 sudo mkdir -p $DATA_PATH
-./run-container.sh
+
+sudo docker run --rm \
+    --name pg-docker-$LONG_APP_NAME \
+    -e POSTGRES_PASSWORD=$PG_PASSWORD \
+    -e LONG_APP_NAME=$LONG_APP_NAME \
+    -d -p $DB_PORT:5432 \
+    -v $DATA_PATH:/var/lib/postgresql/data \
+    postgres:$LONG_APP_NAME
--- docker/scripts/run-container.sh
+++ docker/scripts/run-container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#sudo mkdir -p $DATA_PATH
 sudo docker run --rm \
     --name pg-docker-$LONG_APP_NAME \
     -e POSTGRES_PASSWORD=$PG_PASSWORD \
