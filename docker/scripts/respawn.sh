722ba76191517b1207cf22d4357d23bfcfe43af3
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-22 14:11:49 +0100

    Path

--- docker/scripts/respawn.sh
+++ docker/scripts/respawn.sh
@@ -10,9 +10,9 @@ mkdir -p $HOME/.docker-utils
 cd $HOME/.dockerutils
 sudo rm -r $HOME/.dockerutils/postgresql-utils
 git clone https://github.com/gdeignacio/postgresql-utils.git
-cd $HOME/.dockerutils/postgresql-utils/src
+cd $HOME/.dockerutils/postgresql-utils/docker/src
 sudo docker build -t "postgres:$LONG_APP_NAME" .
-cd $HOME/.dockerutils/postgresql-utils/scripts
+cd $HOME/.dockerutils/postgresql-utils/docker/scripts
 sudo chmod +x *
 #./run-build.sh
 sudo mkdir -p $DATA_PATH
