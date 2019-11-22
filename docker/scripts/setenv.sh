7c108ff16a6c21cd1edf9d51f7b7050aef24c50e
gdeignacio <gdeignacio@gmail.com>
2019-11-22 19:40:25 +0100

    SETENV

--- docker/scripts/setenv.sh
+++ docker/scripts/setenv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export LONG_APP_NAME=custom
-export SHORT_APP_NAME=cst
+export LONG_APP_NAME=portafib
+export SHORT_APP_NAME=pfi
 export DB_PORT=5435
-export PG_PASSWORD=custom
+export PG_PASSWORD=portafib
 export DATA_PATH=/app/docker/postgresql/$LONG_APP_NAME/data
 export TABLESPACE_PATH=$DATA_PATH/tablespaces
 export PGTABLESPACES=/var/lib/postgresql/data/tablespaces
