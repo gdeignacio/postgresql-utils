5d73aca79850fadca5ab0dd35654555d56ae1adb
gdeignacio <gdeignacio@gmail.com>
2019-11-24 23:20:00 +0100

    Dockerfile

--- docker/scripts/respawn.sh
+++ docker/scripts/respawn.sh
@@ -4,32 +4,44 @@
 # All your data will be LOST
 # Run this script only for development
 
-#source ./setenv.sh
+echo ""
+echo respawn.sh started
+echo ""
 
-export LONG_APP_NAME=portafib
-export SHORT_APP_NAME=pfi
-export DB_PORT=5435
-export PG_PASSWORD=portafib
-export DATA_PATH=/app/docker/postgresql/$LONG_APP_NAME/data
-export TABLESPACE_PATH=$DATA_PATH/tablespaces
-export PGTABLESPACES=/var/lib/postgresql/data/tablespaces
+source ./setenv.sh
 
+echo LONG_APP_NAME $LONG_APP_NAME
+echo SHORT_APP_NAME $SHORT_APP_NAME
+echo BD_PORT $DB_PORT
+echo PG_PASSWORD $PG_PASSWORD
+echo DATA_PATH $DATA_PATH
+echo TABLESPACE_PATH $TABLESPACE_PATH
+echo PGTABLESPACES $PGTABLESPACES
+
+# Don't optimize with $DATA_PATH
+# Could be empty and all data will be lost
 sudo rm -r /app/docker/postgresql/$LONG_APP_NAME
 mkdir -p $HOME/.docker-utils
 cd $HOME/.docker-utils
+echo Current dir should be $HOME/.docker-utils
+echo Current dir is `pwd`
+echo `pwd` contains
+ls -l
+echo Removing postgresql-utils from `pwd`
 sudo rm -r $HOME/.docker-utils/postgresql-utils
+ls -l
 git clone https://github.com/gdeignacio/postgresql-utils.git
+echo Cloning from git
 cd $HOME/.docker-utils/postgresql-utils/docker/src
+echo Building images at `pwd`
 sudo docker build -t "postgres:$LONG_APP_NAME" .
 cd $HOME/.docker-utils/postgresql-utils/docker/scripts
+echo Scripts folder `pwd`
 sudo chmod +x *
-#./run-build.sh
-sudo mkdir -p $DATA_PATH
 
-sudo docker run --rm \
-    --name pg-docker-$LONG_APP_NAME \
-    -e POSTGRES_PASSWORD=$PG_PASSWORD \
-    -e LONG_APP_NAME=$LONG_APP_NAME \
-    -d -p $DB_PORT:5432 \
-    -v $DATA_PATH:/var/lib/postgresql/data \
-    postgres:$LONG_APP_NAME
+echo Creating $DATA_PATH folder
+sudo mkdir -p $DATA_PATH
+ls -l $DATA_PATH/..
+sudo docker stop pg-docker-$LONG_APP_NAME
+./run-build.sh
+
--- docker/src/initdb.sh
+++ docker/src/initdb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ./initdb.d/00-setenv.sh
 ./initdb.d/11-create-tablespace-folder.sh
 ./initdb.d/21-create-database.sh
 ./initdb.d/22-create-schema.sh
