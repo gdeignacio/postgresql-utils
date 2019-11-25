b33945a700e0012e1a3323dca1d432af0729ea12
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-25 12:44:36 +0100

    Permisos de ejecución

--- docker/src/initdb.sh
+++ docker/src/initdb.sh
@@ -2,6 +2,8 @@
 
 chmod +x ./initdb.d/*.sh
 source ./initdb.d/00-setenv.sh
+touch asdepicas.txt
+touch asde$APP_DATABASE_NAME.txt
 ./initdb.d/11-create-tablespace-folder.sh
 ./initdb.d/21-create-database.sh
 ./initdb.d/22-create-schema.sh
