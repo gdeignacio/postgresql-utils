e68f5acb56c5abc30a92bb834c08f0b4414fe3b6
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-25 10:16:56 +0100

    Print variables

--- docker/scripts/run-container.sh
+++ docker/scripts/run-container.sh
@@ -2,6 +2,20 @@
 
 #docker exec -i -t [CONTAINER_NAME] /bin/bash
 
+echo ""
+echo run-container.sh started
+echo ""
+
+#source ./setenv.sh
+
+echo LONG_APP_NAME $LONG_APP_NAME
+echo SHORT_APP_NAME $SHORT_APP_NAME
+echo BD_PORT $DB_PORT
+echo PG_PASSWORD $PG_PASSWORD
+echo DATA_PATH $DATA_PATH
+echo TABLESPACE_PATH $TABLESPACE_PATH
+echo PGTABLESPACES $PGTABLESPACES
+
 sudo docker run --rm \
     --name pg-docker-$LONG_APP_NAME \
     -e POSTGRES_PASSWORD=$PG_PASSWORD \
