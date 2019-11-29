6977978b1efa21bf90da6ec26c0586fd0db4eb8e
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2019-11-29 13:15:58 +0100

    Initdb scripts debug

--- docker/src/00_initdb.sh
+++ docker/src/00_initdb.sh
@@ -6,7 +6,7 @@ echo $INIT_CURDIR > $PGDATA/extintor.txt
 
 source /docker-entrypoint-initdb.d/initdb.d/00-setenv.sh
 source /docker-entrypoint-initdb.d/initdb.d/11-create-tablespace-folder.sh
-source /docker-entrypoint-initdb.d/initdb.d/initdb.d/21-create-database.sh
+source /docker-entrypoint-initdb.d/initdb.d/21-create-database.sh
 source /docker-entrypoint-initdb.d/initdb.d/22-create-schema.sh
 
 cd /
